1338be7e639e5467f4489a3201c7ff9e9f824a10
krishanathep@gmail.com <ap4imanee@gmail.com>
2022-08-02 09:13:18 +0700

    First deploy

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <h4>Hello World!</h4>
+    <p>Welcome to this beautiful admin panel.</p>
+    <button class="btn btn-primary">Primary</button>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
